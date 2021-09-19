6f17ecd55502f54518ae435876ae950a07872ff3
Sohaib Aslam <sohaib.aslam@arbisoft.com>
2021-09-19 23:16:59 +0500

    Initial implementation for use with mouse pointers

---
+++ src/components/App/index.tsx
@@ -0,0 +1,45 @@
+import React, {FC, useState} from 'react';
+import Draggable from "react-draggable";
+import './styles.css'
+
+const App: FC = () => {
+  const [xPosition, setXPosition] = useState(10);
+  const [yPosition, setYPosition] = useState(160);
+
+  const handlePositionUpdate = (event: any) => {
+    // Adding 3px to compensate for the mismatch in offset received from mouse event
+    console.log(event)
+    const offSetX = event.offsetX < event.x ? event.offsetX + 3 : 0
+    const offSetY = event.offsetY < event.y ? event.offsetY + 3 : 0
+
+    setXPosition(event.x - offSetX)
+    setYPosition(event.y - offSetY)
+  }
+
+  const handleXInput = (event: any) => {
+    setXPosition(event.target.value)
+  }
+
+  const handleYInput = (event: any) => {
+    setYPosition(event.target.value)
+  }
+
+  return (
+    <>
+      <div className={'input-axis'}>
+        <h1>A draggable box</h1>
+        <input type='number' title={'X-axis'} value={xPosition} onChange={handleXInput}/>
+        <input type='number' title={'Y-axis'} value={yPosition} onChange={handleYInput}/>
+      </div>
+      <Draggable
+        handle=".handle"
+        position={{x: xPosition, y: yPosition}}
+        onDrag={(event) => {handlePositionUpdate(event)}}
+      >
+        <div className="handle draggable" />
+      </Draggable>
+    </>
+  )
+}
+
+export default App;
